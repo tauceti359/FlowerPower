a9011c3024c38bcf9f7f3822ef8ac6c61dd046b2
ZakorBela <blero3311@gmail.com>
2014-12-06 01:44:22 +0100

    most már lövésem sincs mi és miért nem mûködik.. utolsó három indításra három még sosem látott hibát dobott totál random, függõen attól, hogy telefonon vagy AVD fut.. SharedPreferences logikáját elvileg megértettem, mégis StringToInt problémával elszáll az edit.putInt()

--- app/src/main/java/abukottmegalanyok/nik/uniobuda/hu/flowerpower/PrefsActivity.java
+++ app/src/main/java/abukottmegalanyok/nik/uniobuda/hu/flowerpower/PrefsActivity.java
@@ -1,13 +1,42 @@
 package abukottmegalanyok.nik.uniobuda.hu.flowerpower;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
 
-public class PrefsActivity extends PreferenceActivity {
+public class PrefsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        settings.registerOnSharedPreferenceChangeListener( this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        //prefs-ben lévő, key kulcsú beállítás megváltozásának lekezelése
+
+        //Init
+        SharedPreferences sp = sharedPreferences;
+        String key = s;
+        int defValue = 1;
+        //PutInt
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt(key, sp.getInt(key, defValue));
+
+        editor.commit();
+
+        //Toast
+        String toastOut = sp.getString("listpref", "Default");
+        Toast.makeText(this, toastOut, Toast.LENGTH_SHORT).show();
+
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //String toastOut = sp.getString("listpref", "Default");
+        //Toast.makeText(this, toastOut, Toast.LENGTH_SHORT).show();
+
     }
 }
